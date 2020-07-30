8ed46f49e1500c658df019b4eaff163936893ec1
Arlindo jose <arlindojosboa@gmail.com>
2020-07-30 15:01:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## React.js com TypesCript app
+The app loads products from api https://rocketseat-node.herokuapp.com/api and displays on the screen. on click of  button "Acessar" shows the product description.
+
+In the project directory, you can run:
+
+### `yarn start`
+### `yarn build`
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { RouteComponentProps } from 'react-router-dom'
+import api from '../../services/api';
+import './styles.css';
+
+
+type MyProps = RouteComponentProps<{ id?: string }>;
+
+export default class Product extends Component<MyProps> {
+    state = {
+        product: {
+            title: '',
+            description: '',
+            url: ''
+        },
+    }
+    
+    async componentDidMount() {
+        const { id } = this.props.match.params;
+
+        const response = await api.get(`/products/${id}`);
+        
+        this.setState({product: response.data})
+    }
+    render () {
+        const { product } = this.state;
+        return (
+            <div className="product-info">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+
+                <p>
+                    URL: <a href={product.url}>{product.url}</a>
+                </p>
+            </div>
+        )
+    }
+}
